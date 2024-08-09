946079a04d1935fcb25087f3dafe55c6d0b392b8
Haris Mohanty <harismohanty8658@gmail.com>
2024-08-09 12:14:11 +0530

    client- shadcn ui and vite configured successfully.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Hello</Button>
+      <h1 className="text-blue-500">hudhbd</h1>
+    </>
+  );
+}
+
+export default App;
